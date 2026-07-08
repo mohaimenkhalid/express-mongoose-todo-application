8d2e17c3d54cf2f47efe7d837fd49500b8eece58
mohaimen <mohaimen707@gmail.com>
2026-07-09 01:03:07 +0600

    feat(auth): implement user login with jwt

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authController = require('../controllers/auth.controller');
 
 router.post("/signup", authController.signup)
+router.post("/login", authController.signIn)
 
 
 
